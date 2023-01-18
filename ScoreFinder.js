db466b604a7bf3771280580cebcef51e323fa64f
Soumil Maitra <soumilmaitra414@gmail.com>
2023-01-18 12:53:24 +0530

    chores: add eslint

--- ScoreFinder.js
+++ ScoreFinder.js
@@ -1,56 +1,56 @@
 let getScore = (nums) =>{
-    let ans=0;
-    let len=nums.length;
-    let fmeCount=0;
-    let flag=0;
-    for(let i=0;i<len && fmeCount<10;){
-        let fmeScore=nums[i];
-        if(nums[i]==10){
-            if(i+2>=len){
-                flag=1;
-                break;
-            }
-            fmeScore+=nums[i+1]+nums[i+2];
-            i++;
-            fmeCount++;
-        }
-        else{
-            if(i+1>=len){
-                flag=1;
-                break;
-            }
-            if(nums[i]+nums[i+1]==10){
-                if(i+2>=len){
-                    flag=1;
-                    break;
-                }
-                fmeScore+=nums[i+1]+nums[i+2];
-                i+=2;
-                fmeCount++;
-            }
-            else{
-                fmeScore+=nums[i+1];
-                i+=2;
-                fmeCount++;
-            }
-        }
-        ans+=fmeScore;
-    }
-    if(fmeCount<10 || flag==1){
-        throw new Error('Game Incomplete');
-    }
-    return ans;
-}
+	let ans=0;
+	let len=nums.length;
+	let fmeCount=0;
+	let flag=0;
+	for(let i=0;i<len && fmeCount<10;){
+		let fmeScore=nums[i];
+		if(nums[i]==10){
+			if(i+2>=len){
+				flag=1;
+				break;
+			}
+			fmeScore+=nums[i+1]+nums[i+2];
+			i++;
+			fmeCount++;
+		}
+		else{
+			if(i+1>=len){
+				flag=1;
+				break;
+			}
+			if(nums[i]+nums[i+1]==10){
+				if(i+2>=len){
+					flag=1;
+					break;
+				}
+				fmeScore+=nums[i+1]+nums[i+2];
+				i+=2;
+				fmeCount++;
+			}
+			else{
+				fmeScore+=nums[i+1];
+				i+=2;
+				fmeCount++;
+			}
+		}
+		ans+=fmeScore;
+	}
+	if(fmeCount<10 || flag==1){
+		throw new Error('Game Incomplete');
+	}
+	return ans;
+};
 
 let getBestScore = (game_arr)=>{
-    let scoresArray=[];
-    let len=game_arr.length;
-    for(let i=0;i<len;i++){
-        scoresArray.push(getScore(game_arr[i]));
-    }
-    let max_score=max = Math.max.apply(null, scoresArray);
-    return max_score;
-}
+	let scoresArray=[];
+	let len=game_arr.length;
+	for(let i=0;i<len;i++){
+		scoresArray.push(getScore(game_arr[i]));
+	}
+	let max_score= Math.max.apply(null, scoresArray);
+	return max_score;
+};
 
 // console.log(getScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6,3,6]));
 // // console.log(getScore([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]));
